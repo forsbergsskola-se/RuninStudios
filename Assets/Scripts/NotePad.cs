eca5df1740884b412c2676bc420cb55a31b4ccc8
Angelo Panghulan <angelo.panghulan@student.forsbergsskola.se>
2024-11-05 14:48:19 +0100

    Edited functions
    - Functions now disable gameobject, corresponding to correct button pressed

--- Assets/Scripts/NotePad.cs
+++ Assets/Scripts/NotePad.cs
@@ -5,12 +5,30 @@ using UnityEngine;
 
 public class NotePad : MonoBehaviour
 {
+    private GameObject redNote;
+    private GameObject blueNote;
+    private GameObject greenNote;
+    
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Note")
         {
             TriggerFlip(other, true);
             Debug.Log($"{other.GetComponent<Note>().ID}: Trigger ON!");
+            
+            switch(other.GetComponent<Note>().ID)
+            {
+                case "Red":
+                    redNote = other.gameObject;
+                    break;
+                case "Blue":
+                    blueNote = other.gameObject;
+                    break;
+                case "Green":
+                    greenNote = other.gameObject;
+                    break;
+            }
         }
     }
 
@@ -26,22 +44,58 @@ public class NotePad : MonoBehaviour
     private void TriggerFlip(Collider other, bool state)
     {
         other.GetComponent<Note>().isOnTrigger = state;
+        if (state)
+        {
+            switch (other.GetComponent<Note>().ID)
+            {
+                case "RED":
+                    redNote = other.gameObject;
+                    break;
+                case "BLUE":
+                    blueNote = other.gameObject;
+                    break;
+                case "GREEN":
+                    greenNote = other.gameObject;
+                    break;
+            }
+        }
+        else
+        {
+            switch(other.GetComponent<Note>().ID)
+            {
+                case "Red":
+                    redNote = null;
+                    break;
+                case "Blue":
+                    blueNote = null;
+                    break;
+                case "Green":
+                    greenNote = null;
+                    break;
+            }
+        }
     }
 
     public void NotePadPressedRed()
     {
-        Debug.Log("Red Button Pressed!");
-
+        if (redNote.GetComponent<Note>().isOnTrigger)
+        {
+            redNote.SetActive(false);  // Disable the red note GameObject
+        }
     } 
     public void NotePadPressedBlue()
     {
-        Debug.Log("Blue Button Pressed!");
-
+        if (blueNote.GetComponent<Note>().isOnTrigger)
+        {
+            blueNote.SetActive(false);  // Disable the blue note GameObject
+        }
     }
     public void NotePadPressedGreen()
     {
-        Debug.Log("Green Button Pressed!");
-
+        if (greenNote.GetComponent<Note>().isOnTrigger)
+        {
+            greenNote.SetActive(false);  // Disable the green note GameObject
+        }
     }
 
 }
