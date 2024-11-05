df480f8f68e279b751b674d47c212a9813b9799c
Unni <145962681+Iiinnu@users.noreply.github.com>
2024-11-05 15:28:51 +0100

    Working on the Note script so that they can be used for object pooling

--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,7 +9,28 @@ public class Note : MonoBehaviour
     public float speed = 5f;                    // Speed of movement
     public bool isOnTrigger;
     public string ID;
+    private int destroyAfter = 5; //Trigger this when the button har pressed correctly 
+    private ParticleSystem particle;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        Destroy(gameObject, destroyAfter);
+    }
+
+    public void Hit()
+    {
+        ParticleSystem explosion = Instantiate(particle, transform.position, Quaternion.identity);
+        Destroy(explosion.gameObject, destroyAfter);
+        Destroy(gameObject);
+    }
     
+
     void Update()
     {
         Move();
