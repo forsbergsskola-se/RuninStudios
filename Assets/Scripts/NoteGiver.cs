6b81a86df97ff0694b69cce77ac5f0f7e5c40869
reynin <144011447+reynin@users.noreply.github.com>
2024-11-06 23:02:37 +0100

    Edited function on NoteGiver prefab + Script
    - SpawnNote() function now randomize the 3 types of notes and and sends them on respective trackline

--- Assets/Scripts/NoteGiver.cs
+++ Assets/Scripts/NoteGiver.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class NoteGiver : MonoBehaviour
 
 {
     [SerializeField] private Note notePrefab;
+    [SerializeField] private GameObject redSlot;
+    [SerializeField] private GameObject blueSlot;
+    [SerializeField] private GameObject greenSlot;
+    
     public float noteSpawnInterval = 1f;
     private float noteSpawnTimer = 0f;
 
@@ -22,8 +27,26 @@ public class NoteGiver : MonoBehaviour
     
     private void SpawnNote()
     {
-        Note note = Instantiate(notePrefab,transform.position,Quaternion.Euler(transform.rotation.x, transform.rotation.y+180, transform.rotation.z)); // Instantiate the Note prefab 
-        note.ID = "BLUE";
-        note.gameObject.SetActive(true);
+        int randomIndex = Random.Range(0, 3);
+        switch (randomIndex)
+        {
+            case 0:
+                Note redNote = Instantiate(notePrefab,redSlot.transform.position,Quaternion.Euler(transform.rotation.x, transform.rotation.y+180, transform.rotation.z)); // Instantiate the Note prefab 
+                redNote.ID = "RED";
+                redNote.gameObject.SetActive(true);
+                break; 
+            case 1:
+                Note blueNote = Instantiate(notePrefab,blueSlot.transform.position,Quaternion.Euler(transform.rotation.x, transform.rotation.y+180, transform.rotation.z)); // Instantiate the Note prefab 
+                blueNote.ID = "BLUE";
+                blueNote.gameObject.SetActive(true);
+                break;
+            case 2:
+                Note greenNote = Instantiate(notePrefab,greenSlot.transform.position,Quaternion.Euler(transform.rotation.x, transform.rotation.y+180, transform.rotation.z)); // Instantiate the Note prefab 
+                greenNote.ID = "BLUE";
+                greenNote.gameObject.SetActive(true);
+                break;
+        }
+        
+        
     }
 }
