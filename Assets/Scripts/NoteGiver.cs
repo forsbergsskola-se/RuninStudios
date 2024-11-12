8c136eefd944663d5acecebd5817aabf2dff9d26
Angelo Panghulan <angelo.panghulan@student.forsbergsskola.se>
2024-11-12 10:10:18 +0100

    "Revert" back to old code for testing
    - Reason being for checking inputs delays and timing with functions and gameobjects

--- Assets/Scripts/NoteGiver.cs
+++ Assets/Scripts/NoteGiver.cs
@@ -7,14 +7,58 @@ using Random = UnityEngine.Random;
 public class NoteGiver : MonoBehaviour
 
 {
+    [SerializeField] private Note notePrefab;
     [SerializeField] private GameObject redSlot;
+    [SerializeField] private GameObject blueSlot;
+    [SerializeField] private GameObject greenSlot;
+    
+    public float noteSpawnInterval = 1f;
+    private float noteSpawnTimer = 0f;
+
+    private void Update()
+    {
+        noteSpawnTimer += Time.deltaTime;
+        if (noteSpawnTimer >= noteSpawnInterval)
+        {
+            noteSpawnTimer = 0f;
+            SpawnNote();
+        }
+    }
+    
+    private void SpawnNote()
+    {
+        Quaternion slotRotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y + 180, transform.rotation.z);
+
+        int randomIndex = Random.Range(0, 3);
+        switch (randomIndex)
+        {
+            case 0:
+                Note redNote = Instantiate(notePrefab,redSlot.transform.position,slotRotation); // Instantiate the Note prefab 
+                redNote.ID = "RED";
+                redNote.gameObject.SetActive(true);
+                break; 
+            case 1:
+                Note blueNote = Instantiate(notePrefab,blueSlot.transform.position,slotRotation); // Instantiate the Note prefab 
+                blueNote.ID = "BLUE";
+                blueNote.gameObject.SetActive(true);
+                break;
+            case 2:
+                Note greenNote = Instantiate(notePrefab,greenSlot.transform.position,slotRotation); // Instantiate the Note prefab 
+                greenNote.ID = "GREEN";
+                greenNote.gameObject.SetActive(true);
+                break;
+        }
+        
+        
+    }
+    /*[SerializeField] private GameObject redSlot;
     [SerializeField] private GameObject blueSlot;
     [SerializeField] private GameObject greenSlot;
 
     public void SendNote(Note note, string ID)
     {
-        Quaternion slotRotation =
-            Quaternion.Euler(transform.rotation.x, transform.rotation.y + 180, transform.rotation.z);
+        Quaternion slotRotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y + 180, transform.rotation.z);
+        
         switch (ID)
         {
             case "RED":
@@ -31,6 +75,5 @@ public class NoteGiver : MonoBehaviour
                 break;
         }
         note.ID = ID;
-
-    }
+    }*/
 }
--- Assets/Scripts/SoundtrackManager.cs
+++ Assets/Scripts/SoundtrackManager.cs
@@ -14,7 +14,7 @@ public class SoundtrackManager : MonoBehaviour
     public NoteGiver noteGiver;
     [SerializeField] private Note notePrefab;
 
-    void Start()
+    /*void Start()
     {
         // Create the event instance and start it
         soundTrackInstance = RuntimeManager.CreateInstance(soundTrackEvent);
@@ -26,12 +26,12 @@ public class SoundtrackManager : MonoBehaviour
         soundTrackInstance.start();
     }
 
-    private FMOD.RESULT OnSoundtrackEventCallback(FMOD.Studio.EVENT_CALLBACK_TYPE type, IntPtr intPtr, IntPtr parameterPtr)
+    private FMOD.RESULT OnSoundtrackEventCallback(EVENT_CALLBACK_TYPE type, IntPtr intPtr, IntPtr parameterPtr)
     {
         // Check if this callback is for a marker
-        if (type == FMOD.Studio.EVENT_CALLBACK_TYPE.TIMELINE_MARKER)
+        if (type == EVENT_CALLBACK_TYPE.TIMELINE_MARKER)
         {
-            var marker = (FMOD.Studio.TIMELINE_MARKER_PROPERTIES)Marshal.PtrToStructure(parameterPtr, typeof(FMOD.Studio.TIMELINE_MARKER_PROPERTIES));
+            var marker = (TIMELINE_MARKER_PROPERTIES)Marshal.PtrToStructure(parameterPtr, typeof(FMOD.Studio.TIMELINE_MARKER_PROPERTIES));
 
             Debug.Log("Marker reached: " + marker.name);
 
@@ -51,11 +51,11 @@ public class SoundtrackManager : MonoBehaviour
         }
         return FMOD.RESULT.OK;
     }
-
+    
     void OnDestroy()
     {
         // Stop the soundtrack instance and release it
         soundTrackInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
         soundTrackInstance.release();
-    }
+    }*/
 }
