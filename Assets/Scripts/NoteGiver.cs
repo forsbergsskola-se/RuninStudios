3e62be2097bb89b3f0f95aac498c0b0f9c302ac3
Unni <145962681+Iiinnu@users.noreply.github.com>
2024-11-12 14:25:32 +0100

    updating not giver script and trying to attache it to the song manager script so that the notes are set out with the beat

--- Assets/Scripts/NoteGiver.cs
+++ Assets/Scripts/NoteGiver.cs
@@ -15,6 +15,21 @@ public class NoteGiver : MonoBehaviour
     
     public float noteSpawnInterval = 1f;
     private float noteSpawnTimer = 0f;
+    private List<Note> notePool;
+    private int poolSize;
+
+
+    private void Start()
+    {
+        notePool = new List<Note>();
+
+        for (int i = 0; i < poolSize; i++)
+        {
+            Note note = Instantiate(notePrefab, transform);
+            note.gameObject.SetActive(false);
+            notePool.Add(note);
+        }
+    }
 
     private void Update()
     {
@@ -25,34 +40,57 @@ public class NoteGiver : MonoBehaviour
             SpawnNote();
         }
     }
+
+    private Note GetPooledNote()
+    {
+        foreach (Note note in notePool)
+        {
+            if (!note.gameObject.activeInHierarchy)
+            {
+                return note;
+            }
+        }
+        return null;
+    }
     
     private void SpawnNote()
     {
-        Quaternion slotRotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y + 180, transform.rotation.z);
-
-        int randomIndex = Random.Range(0, 3);
-        switch (randomIndex)
+        
+        int randomIndex = Random.Range(0, slots.Length);
+        Note newNote = GetPooledNote();
+        if (newNote != null )
         {
-            case 0:
-                Note redNote = Instantiate(notePrefab,redSlot.transform.position,slotRotation); // Instantiate the Note prefab 
-                redNote.ID = "RED";
-                redNote.gameObject.SetActive(true);
-                break; 
-            case 1:
-                Note blueNote = Instantiate(notePrefab,blueSlot.transform.position,slotRotation); // Instantiate the Note prefab 
-                blueNote.ID = "BLUE";
-                blueNote.gameObject.SetActive(true);
-                break;
-            case 2:
-                Note greenNote = Instantiate(notePrefab,greenSlot.transform.position,slotRotation); // Instantiate the Note prefab 
-                greenNote.ID = "GREEN";
-                greenNote.gameObject.SetActive(true);
-                break;
+            newNote.ResetNote(slots[randomIndex].transform.position, slots[randomIndex].name.ToUpper());
         }
-        
-        
     }
-    /*[SerializeField] private GameObject redSlot;
+    /*
+     
+     Quaternion slotRotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y + 180, transform.rotation.z);
+
+       int randomIndex = Random.Range(0, 3);
+       switch (randomIndex)
+       {
+           case 0:
+               Note redNote = Instantiate(notePrefab,redSlot.transform.position,slotRotation); // Instantiate the Note prefab 
+               redNote.ID = "RED";
+               redNote.gameObject.SetActive(true);
+               break; 
+           case 1:
+               Note blueNote = Instantiate(notePrefab,blueSlot.transform.position,slotRotation); // Instantiate the Note prefab 
+               blueNote.ID = "BLUE";
+               blueNote.gameObject.SetActive(true);
+               break;
+           case 2:
+               Note greenNote = Instantiate(notePrefab,greenSlot.transform.position,slotRotation); // Instantiate the Note prefab 
+               greenNote.ID = "GREEN";
+               greenNote.gameObject.SetActive(true);
+               break;
+       }
+     
+     
+     
+     
+     [SerializeField] private GameObject redSlot;
     [SerializeField] private GameObject blueSlot;
     [SerializeField] private GameObject greenSlot;
 
