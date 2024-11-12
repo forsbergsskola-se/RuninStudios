9201d990a38da5bd2038650bf6f3b9d75df8a611
Unni <145962681+Iiinnu@users.noreply.github.com>
2024-11-12 11:58:57 +0100

    Added songmanager script

---
+++ Assets/Scripts/SongManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SongManager : MonoBehaviour
+{
+    
+    float songPosition;//the current position of the song (in seconds)
+    
+    float songPosInBeats;//the current position of the song (in beats)
+   
+    float secPerBeat; //the duration of a beat
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
