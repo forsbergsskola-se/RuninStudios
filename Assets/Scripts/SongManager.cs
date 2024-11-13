75034d988637859935acfd414637a16b60d2b0cf
Unni <145962681+Iiinnu@users.noreply.github.com>
2024-11-13 11:47:58 +0100

    Adding [SerializeField] on bpm and notes array so that we can manually test to song and add nots when fit. Added audio source and song to song manager

--- Assets/Scripts/SongManager.cs
+++ Assets/Scripts/SongManager.cs
@@ -4,25 +4,23 @@ using UnityEngine;
 
 public class SongManager : MonoBehaviour
 {
-    
+    public AudioSource audioSource;
     float songPosition;//the current position of the song (in seconds)
-    
     float songPosInBeats;//the current position of the song (in beats)
-   
     float secPerBeat; //the duration of a beat
-    
     float dsptimesong; // Time (in seconds) when the song started
-    float bpm; // Beats per minute of the song
-    float[] notes; // Array of note positions in beats
+    [SerializeField] float bpm = 86f; // Beats per minute of the song
+    [SerializeField] float[] notes; // timing info in terms of the beat of the song
     int nextIndex = 0; // Index of the next note to be spawned
     [SerializeField] private NoteGiver noteGiver;
     
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         secPerBeat = 60f / bpm; //calculated secunds per beat
         dsptimesong = (float)AudioSettings.dspTime; //record the start time
-        GetComponent<AudioSource>().Play();//starts the song
+        audioSource.Play();
+        Debug.Log("Song started");
     }
 
     // Update is called once per frame
