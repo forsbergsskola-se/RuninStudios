ed3423329ab3b18c7e861942617ccb6e8a769ee6
Angelo Panghulan <angelo.panghulan@student.forsbergsskola.se>
2024-12-05 14:08:54 +0100

    Rework on Network.cs
    - Setup a steppin stone for implement leaderboards system

--- Assets/Scripts/Backend/Network.cs
+++ Assets/Scripts/Backend/Network.cs
@@ -11,6 +11,16 @@ namespace Backend
         public static Network sharedInstance;
         private static GlobalData globalData;
         private static UserData userData;
+        private static LeaderboardData leaderboardIDOneData;
+        private static LeaderboardData leaderboardIDTwoData;
+        private static LeaderboardData leaderboardIDThreeData;
+
+        private string leaderboardIDOne = "SongOne";
+        private string leaderboardIDTwo = "SongTwo";
+        private string leaderboardIDThree = "SongThree";
+        BrainCloudSocialLeaderboard.SortOrder sortOrder = BrainCloudSocialLeaderboard.SortOrder.HIGH_TO_LOW;
+        int startIndex = 0;
+        int endIndex = 9;
 
         public delegate void AuthenticationRequestCompleted();
         public delegate void AuthenticationRequestFailed();
@@ -65,7 +75,7 @@ namespace Backend
             };
 
             // BrainCloud request
-            bc.AuthenticateAnonymous(successCallback, failureCallback);
+            bc.Client.AuthenticationService.AuthenticateAnonymous(true, successCallback, failureCallback);
         }
         #endregion
 
@@ -80,36 +90,132 @@ namespace Backend
             //Load User Stats
             ReadUserFromServer();
             
+            //Save Read data to local static script
+            //LoadToLocal();
+            
+            // Read all leaderboards from server
+            ReadAllLeaderboardFromServer();
+            
+            //Test posting
+            UploadToLeaderboard(leaderboardIDOne, 10, "{\"nickname\":\"batman\"}");
         }
 
+        //Get Global Data Statistics "Top Global HighScores"
         private void ReadGlobalFromServer()
         {
-            bc.GlobalStatisticsService.ReadAllGlobalStats((response, cbobject) =>
+            //-----Error Handling-----
+            SuccessCallback successCallback = (response, cbObject) =>
             {
-                globalData.data = JsonUtility.FromJson<GlobalData.Data>(response);
-                Debug.Log($"Json Converted! {globalData}");
-            });
+                Debug.Log(string.Format("Success | {0}", response));
+                globalData = JsonUtility.FromJson<GlobalData>(response);
+            };
+            FailureCallback failureCallback = (status, code, error, cbObject) =>
+            {
+                Debug.Log(string.Format("Failed | {0}  {1}  {2}", status, code, error));
+            };
+            bc.GlobalStatisticsService.ReadAllGlobalStats(successCallback, failureCallback);
         }
 
+        //Get User Data Statistics "Top Personal HighScores"
         private void ReadUserFromServer()
+        {
+            //-----Error Handling-----
+            SuccessCallback successCallback = (response, cbObject) =>
+            {
+                Debug.Log(string.Format("Success | {0}", response));
+                userData = JsonUtility.FromJson<UserData>(response);
+            };
+            FailureCallback failureCallback = (status, code, error, cbObject) =>
+            {
+                Debug.Log(string.Format("Failed | {0}  {1}  {2}", status, code, error));
+            };
+            bc.PlayerStatisticsService.ReadAllUserStats(successCallback, failureCallback);
+        }
+
+        //Get Leaderboard Data
+        private void ReadAllLeaderboardFromServer()
+        {
+            ReadLeaderboardID(leaderboardIDOne, leaderboardIDOneData);    
+            ReadLeaderboardID(leaderboardIDTwo, leaderboardIDTwoData);            
+            ReadLeaderboardID(leaderboardIDThree, leaderboardIDThreeData);
+        }
+        
+        //Helper function
+        private void ReadLeaderboardID(string leaderboardID, LeaderboardData leaderboardIDData)
         {
             SuccessCallback successCallback = (response, cbObject) =>
             {
                 Debug.Log(string.Format("Success | {0}", response));
-                userData.data = JsonUtility.FromJson<UserData.Data>(response);
+                leaderboardIDData = JsonUtility.FromJson<LeaderboardData>(response);
+            };
+            FailureCallback failureCallback = (status, code, error, cbObject) =>
+            {
+                Debug.Log(string.Format("Failed | {0}  {1}  {2}", status, code, error));
+            };
+            bc.LeaderboardService.GetGlobalLeaderboardPage(leaderboardID, sortOrder, startIndex, endIndex, successCallback, failureCallback);
+        }
+
+        private void UploadToLeaderboard(string leaderboardID, int score, string moi)
+        {
+            SuccessCallback successCallback = (response, cbObject) =>
+            {
+                Debug.Log(string.Format("Success | {0}", response));
             };
             FailureCallback failureCallback = (status, code, error, cbObject) =>
             {
                 Debug.Log(string.Format("Failed | {0}  {1}  {2}", status, code, error));
             };
 
-            bc.PlayerStatisticsService.ReadAllUserStats(successCallback, failureCallback);
+            bc.LeaderboardService.PostScoreToLeaderboard(leaderboardID, score, moi, successCallback, failureCallback);
         }
         
         //Saving data, send to BrainCloud server
         public static void SaveData()
         {
             
+        }
+        private static void LoadToLocal()
+        {
+            PlayerData.personalScoreOne = userData.data.statistics.ScoreSongOne;
+            PlayerData.personalScoreTwo = userData.data.statistics.ScoreSongTwo;
+            PlayerData.personalScoreThree = userData.data.statistics.ScoreSongThree;
+
+            PlayerData.globalScoreOne = globalData.data.statistics.GlobalHighscoreSongOne;
+            PlayerData.globalScoreTwo = globalData.data.statistics.GlobalHighscoreSongTwo;
+            PlayerData.globalScoreThree = globalData.data.statistics.GlobalHighscoreSongThree;
+        }
+    }
+
+    [Serializable]
+    class LeaderboardData
+    {
+        public Data data;
+        private int status;
+        
+        [Serializable]
+        public class Data
+        {
+            public List<LeaderboardEntry> leaderboard;
+            public bool moreBefore;
+            public bool moreAfter;
+            public long timeBeforeReset;
+            public long server_time;
+        }
+
+        [Serializable]
+        public class LeaderboardEntry
+        {
+            public string playerId;
+            public int score;
+            public string data;
+            public long createdAt;
+            public long updatedAt;
+            public int index;
+            public int rank;
+            public string name;
+            public string summaryFriendData;
+            public string pictureUrl;
+            public bool rewarded;
         }
     }
 
@@ -118,6 +224,8 @@ namespace Backend
     class GlobalData
     {
         public Data data;
+        private int statuse;
+        
         [Serializable]
         public class Data
         {
